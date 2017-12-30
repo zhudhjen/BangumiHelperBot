01a5dd097975ee60b088b62e45b485d1e82a33c3
zhudhjen <zhudhjen@gmail.com>
2017-12-30 15:51:06 +0800

    Build up framework

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ app.user(bodyParser.urlencoded({
     extended: true
 }));
 
+app.post('/new-message', function(req, res) {
+    const {message} = req.body;
 
+    if (!message || message.text.toLowerCase().indexOf('macro') < 0) {
+        return res.end();
+    }
 
+    axios.post('https://api.telegram.org/' + token + '/sendMessage', {
+        chat_id: message.chat.id,
+        text: 'Poi~'
+    })
+    .then(response => {
+        console.log('Message sent');
+        res.end('ok');
+    })
+    .catch(err => {
+        console.log('Error: ', err);
+        res.end('Error: ' + err);
+    });
+});
+
+var port = 14200;
+app.listen(port, function() {
+    console.log('Bangumi Helper Bot listening on port ' + port);
+});
 
